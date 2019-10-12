ca35b638605e49e720e79ac93a75838016a035ef
Duncan Leo <hello@duncanleo.me>
2019-10-12 17:42:39 +0800

    move response models to another file

--- smite/model.go
+++ smite/model.go
@@ -1,27 +1,5 @@
 package smite
 
-// CreateSessionResponse response when creating a session
-type CreateSessionResponse struct {
-	RetMsg    string `json:"ret_msg"`
-	SessionID string `json:"session_id"`
-	Timestamp string `json:"timestamp"`
-}
-
-// GetDataUsedResponse response when getting API usage count
-type GetDataUsedResponse []struct {
-	ActiveSessions     int         `json:"Active_Sessions"`
-	ConcurrentSessions int         `json:"Concurrent_Sessions"`
-	RequestLimitDaily  int         `json:"Request_Limit_Daily"`
-	SessionCap         int         `json:"Session_Cap"`
-	SessionTimeLimit   int         `json:"Session_Time_Limit"`
-	TotalRequestsToday int         `json:"Total_Requests_Today"`
-	TotalSessionsToday int         `json:"Total_Sessions_Today"`
-	RetMsg             interface{} `json:"ret_msg"`
-}
-
-// GetGodsResponse response when getting all gods
-type GetGodsResponse []God
-
 // God is a god character in SMITE
 type God struct {
 	Ability1                   GodAbility  `json:"Ability_1"`
